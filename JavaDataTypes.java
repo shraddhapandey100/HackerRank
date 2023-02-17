175d03378d30b1eea1df9d30f77208211561a678
Shraddha Pandey <85031120+shraddhapandey100@users.noreply.github.com>
2023-02-17 22:56:36 +0530

    Create JavaDataTypes.java

---
+++ JavaDataTypes.java
@@ -0,0 +1,23 @@
+/*
+
+Java has 8 primitive data types; char, boolean, byte, short, int , long, float and double. For this exercise,
+we'll work with the primitives used to hold integer values (byte, short, int and long):
+
+A byte is an 8-bit signed integer.
+A short is a 16-bit signed integer.
+A int is a 32-bit signed integer.
+A long is a 64-bit signed integer.
+
+Input Format: The first line contains an integer, T, denoting the number of test cases.
+Each test case, T, is comprised of a single line with an integer, n, which can be arbitrarily larger
+or small.
+Output Format: For each input variable n and appropriate primitive dataType, you must determine if the
+given primitives are capable of storing it. If yes, the print.
+
+If there is more than one appropriate type, print each one on its own line and order them by size.(i.e. 
+byte <short<int<long.
+
+If the number cannot be stored in one of the four aforementioned primitive print the line.
+
+
+*/
