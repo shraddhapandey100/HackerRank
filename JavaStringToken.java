d77f68210451cfbb5bb0b98c5d7e23b0866994f7
Shraddha Pandey <85031120+shraddhapandey100@users.noreply.github.com>
2023-02-21 03:24:56 +0530

    Create JavaStringToken.java

---
+++ JavaStringToken.java
@@ -0,0 +1,19 @@
+/*
+Given a string s, matching the regular expression [A-Za-z!,?._'@]+, split the string into tokens. 
+Print the number of tokens, followed by each token on a new line.
+The string.split() method breaks a given string aroud matches of the given regular expression.
+After splitting against the given regular expression, this method returns a string array.
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Solution{
+  public static void main(String[] args){
+      Scanner scan = new Scanner(System.in);
+      String s = scan.nextLine();
+      S
+  
+  }
+
+}
