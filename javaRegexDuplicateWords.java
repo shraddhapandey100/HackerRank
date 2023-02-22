adba7029a5928f0c449f7c0f9eccee41363ad018
Shraddha Pandey <85031120+shraddhapandey100@users.noreply.github.com>
2023-02-22 13:04:55 +0530

    Create javaRegexDuplicateWords.java

---
+++ javaRegexDuplicateWords.java
@@ -0,0 +1,17 @@
+/*
+
+Input Format: 
+The following input is handled for you the given stub code:
+The first line contains an integer, n, denoting the number of sentences.
+Each of the n subsequent lines contains a single sentence consisting of English albhabetic letters
+and letters and whitespaces characters.
+
+Consntraints: 
+Each sentence consists of at most 10^4 english alphabetic letters and whitespaces.
+1<=n<=100.
+
+Output Format: 
+Stub code in the editor prints the sentence modified by the replaceAll line to stdout. The modified 
+string must be a modified version of the intial sentence where all repeat occurrences of each word 
+are removed.
+*/
